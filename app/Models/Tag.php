9d8ead3cdcf94fc8dc0734e7b9b99a827a8ad96a
krzysztof-brylski1 <krzysztof.brylski1@o2.pl>
2023-03-13 21:46:10 +0100

    implemented post and tag models and migrations

---
+++ app/Models/Tag.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Tag extends Model
+{
+    use HasFactory;
+
+    protected $fillable=[
+        'name',
+    ];
+
+    public function Posts(){
+        return $this->belongsToMany(Post::class,'posts_tags');
+    }
+}
